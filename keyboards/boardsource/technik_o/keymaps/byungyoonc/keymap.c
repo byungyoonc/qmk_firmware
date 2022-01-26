eda8a93c5c4222b49a2bb022e338123b193e87df
Choi Byungyoon <byungyoonc@gmail.com>
2022-01-27 01:37:32 +0900

    Change keymap in layer GAME_SPC

--- keyboards/boardsource/technik_o/keymaps/byungyoonc/keymap.c
+++ keyboards/boardsource/technik_o/keymaps/byungyoonc/keymap.c
@@ -115,14 +115,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | LSft |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | RSft |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | LCtl |      | LAlt |      | Num1 | Spce | Spce | Num3 | RAlt |      |  Fn  | ADJS |
+ * | LCtl |      | LAlt |      |   F  | Spce | Spce |   J  | RAlt |      |  Fn  | ADJS |
  * `-----------------------------------------------------------------------------------'
  */
 [_GAME_SPC] = LAYOUT_ortho_4x12(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT ,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, XXXXXXX, KC_LALT, XXXXXXX, KC_P1,   KC_SPC,  KC_SPC,  KC_P3,   KC_RALT, XXXXXXX, FUNCTION,ADJUST
+    KC_LCTL, XXXXXXX, KC_LALT, XXXXXXX, KC_F,    KC_SPC,  KC_SPC,  KC_J,    KC_RALT, XXXXXXX, FUNCTION,ADJUST
 ),
 
 /* GAMING WITH WASD, NUMROW
