c551a51e357753f1fe1f7e6c917e5b372e9f78b0
Choi Byungyoon <byungyoonc@gmail.com>
2022-04-29 08:37:14 +0900

    Update Boardwalk personal keymap

--- keyboards/boardwalk/keymaps/byungyoonc/keymap.c
+++ keyboards/boardwalk/keymaps/byungyoonc/keymap.c
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Base Layer
      * .-----------------------------------------------------------------------------------------------------------------------------.
-     * | `      | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      | -      | =      | PRTSCR |
+     * | `      | 1      | 2      | 3      | 4      | F5     | F6     | F7     | F8     | F9     | F10    | F11    | F12    | MENU   |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
-     * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      | BACKSP | BACKSP | DELETE |
+     * | TAB    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      | BACKSP |        | DELETE |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
      * | ESC    | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | '      | ENTER  | HOME   |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT_ortho_5x14(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_PSCR,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_BSPC, KC_DEL,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_APP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, XXXXXXX, KC_DEL,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_HOME,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT, ADJUST,  NUM,     KC_SPC,  KC_SPC,  SYMBOL,  KC_RALT, KC_APP,  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
@@ -120,20 +120,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * .-----------------------------------------------------------------------------------------------------------------------------.
      * |        | SEC1   | SEC2   |        |        |        |        |        |        |        |        |        | DEBUG  | RESET  |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
-     * | F1     |        | VOL+   |        | RG_ADD | NK_TOG |        | F9     | F10    | F11    | F12    |        |        |        |
+     * | F1     |        | VOL+   |        | RG_ADD | NK_TOG |        | F7     | F8     | F9     | F10    |        |        |        |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
-     * |        | RWND   | VOL-   | FFWD   |        |        |        | F5     | F6     | F7     | F8     |        |        |        |
+     * |        | RWND   | VOL-   | FFWD   |        |        |        | F4     | F5     | F6     | F11    |        |        |        |
      * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
-     * |        | PLAY   | MUTE   |        |        |        |        | F1     | F2     | F3     | F4     |        |        |        |
+     * |        | PLAY   | MUTE   |        |        |        |        | F1     | F2     | F3     | F12    |        |        |        |
      * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
      * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
      * '-----------------------------------------------------------------------------------------------------------------------------'
      */
     [_ADJUST] = LAYOUT_ortho_5x14(
         _______, KC_SEC1, KC_SEC2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DEBUG,   RESET,
-        KC_F1,   XXXXXXX, KC_VOLU, XXXXXXX, RG_ADD,  NK_TOGG, XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
-        _______, KC_MRWD, KC_VOLD, KC_MFFD, XXXXXXX, RGB_TOG, XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______,
-        _______, KC_MPLY, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______,
+        KC_F1,   XXXXXXX, KC_VOLU, XXXXXXX, RG_ADD,  NK_TOGG, XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______,
+        _______, KC_MRWD, KC_VOLD, KC_MFFD, XXXXXXX, RGB_TOG, XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, _______, _______,
+        _______, KC_MPLY, KC_MUTE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 
