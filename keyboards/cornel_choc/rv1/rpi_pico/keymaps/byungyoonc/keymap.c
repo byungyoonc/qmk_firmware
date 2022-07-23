ffd25d5fd722502ddecaeaccba197cff8e9be27c
Choi Byungyoon <byungyoonc@gmail.com>
2022-07-23 09:49:49 +0900

    update cornel_choc personal keymap

--- keyboards/cornel_choc/rv1/rpi_pico/keymaps/byungyoonc/keymap.c
+++ keyboards/cornel_choc/rv1/rpi_pico/keymaps/byungyoonc/keymap.c
@@ -1,21 +1,18 @@
 #include QMK_KEYBOARD_H
 #include "byungyoonc.h"
-#include "print.h"
 
 enum custom_layers {
     _BASE,
     _ALTSPACE,
-    _NUM,
+    _NUMNAV,
     _SYM,
-    _NAV,
-    _MOUSEKEY,
     _ADJUST
 };
 
-#define NUM MO(_NUM)
+#define NUMNAV MO(_NUMNAV)
 #define SYM MO(_SYM)
-#define NAV MO(_NAV)
 #define ALTSP TG(_ALTSPACE)
+#define SH_SPC SH_T(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_4x6_3(
@@ -23,41 +20,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            NAV,     NUM,     KC_SPC,  KC_SPC,  SYM,     KC_LALT
+                                            KC_LALT, NUMNAV,  SH_SPC,  KC_SPC,  SYM,     KC_LGUI
     ),
     [_ALTSPACE] = LAYOUT_split_4x6_3(
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-                                            _______, _______, _______, KC_P0,   _______, _______
+                                            _______, _______, KC_SPC,  KC_P0,   _______, _______
     ),
-    [_NUM] = LAYOUT_split_4x6_3(
+    [_NUMNAV] = LAYOUT_split_4x6_3(
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                   _______, KC_7,    KC_8,    KC_9,    _______, _______,
-        _______, _______, _______, _______, _______, _______,                   _______, KC_4,    KC_5,    KC_6,    _______, _______,
-        _______, KC_ESC,  _______, _______, _______, _______,                   _______, KC_1,    KC_2,    KC_3,    KC_PDOT, _______,
+        _______, KC_HOME, KC_UP,   KC_END,  KC_WH_U, XXXXXXX,                   XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX, KC_DEL,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_WH_D, XXXXXXX,                   XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, _______,
+        _______, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_1,    KC_2,    KC_3,    KC_PDOT, _______,
                                             _______, _______, _______, KC_0,    _______, _______
     ),
     [_SYM] = LAYOUT_split_4x6_3(
         _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_PERC, _______,                   _______, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,  _______,
-        _______, KC_AMPR, KC_ASTR, KC_DLR,  _______, _______,                   _______, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______,
-        _______, _______, KC_CIRC, KC_CIRC, _______, _______,                   _______, _______, KC_RALT, _______, _______, _______,
-                                            _______, _______, _______, _______, _______, _______
-    ),
-    [_NAV] = LAYOUT_split_4x6_3(
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        KC_LGUI, _______, _______, _______, _______, _______,                   _______, _______, _______, KC_HOME, _______, _______,
-        _______, _______, _______, _______, _______, _______,                   KC_WH_U, _______, KC_UP,   KC_END,  _______, _______,
-        _______, KC_ESC,  _______, _______, _______, _______,                   KC_WH_D, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-                                            _______, _______, _______, _______, _______, _______
-    ),
-    [_MOUSEKEY] = LAYOUT_split_4x6_3(
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_BTN2, KC_BTN3, KC_BTN1, _______,                   KC_WH_U, _______, KC_MS_U, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                   KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______,
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_PERC, XXXXXXX,                   XXXXXXX, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,  KC_DEL,
+        _______, KC_AMPR, KC_ASTR, KC_DLR,  XXXXXXX, XXXXXXX,                   XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______,
+        _______, XXXXXXX, KC_CIRC, KC_CIRC, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, _______,
                                             _______, _______, _______, _______, _______, _______
     ),
     [_ADJUST] = LAYOUT_split_4x6_3(
@@ -76,7 +59,52 @@ void matrix_output_unselect_delay(uint8_t line, bool key_pressed) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    state = update_tri_layer_state(state, _NUM, _SYM, _ADJUST);
-    state = update_tri_layer_state(state, _NAV, _SYM, _MOUSEKEY);
+    state = update_tri_layer_state(state, _NUMNAV, _SYM, _ADJUST);
     return state;
 }
+
+const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
+  {{0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5} },
+  {{0, 6}, {1, 6}, {2, 6}, {3, 6}, {4, 6}, {5, 6} },
+  {{0, 7}, {1, 7}, {2, 7}, {3, 7}, {4, 7}, {5, 7} },
+  {{0, 8}, {1, 8}, {2, 8}, {3, 8}, {4, 8}, {5, 8} },
+  {{0, 9}, {1, 9}, {2, 9}, {3, 9}, {4, 9}, {5, 9} },
+  {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0} },
+  {{0, 1}, {1, 1}, {2, 1}, {3, 1}, {4, 1}, {5, 1} },
+  {{0, 2}, {1, 2}, {2, 2}, {3, 2}, {4, 2}, {5, 2} },
+  {{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3} },
+  {{0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4} },
+};
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _BASE:
+            oled_write_P(PSTR("QWERTY\n"), false);
+            break;
+        case _NUMNAV:
+            oled_write_P(PSTR("NUMNAV\n"), false);
+            break;
+        case _SYM:
+            oled_write_P(PSTR("SYMBOL\n"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("ADJUST\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return false;
+}
+#endif
