a23f19a6ccb59306b2968d0f480cf20d3ea03bd1
Choi Byungyoon <byungyoonc@gmail.com>
2022-07-28 23:48:21 +0900

    Changed Reviung41 personal keymap

--- keyboards/reviung/reviung41/keymaps/byungyoonc/keymap.c
+++ keyboards/reviung/reviung41/keymaps/byungyoonc/keymap.c
@@ -20,12 +20,14 @@ enum layer_names {
     _BASE,
     _ALTFN,
     _LOWER,
+    _NUMPAD,
     _RAISE,
     _ADJUST,
     _ADJUST2
 };
 
 #define LOWER  MO(_LOWER)
+#define NUMPAD MO(_NUMPAD)
 #define RAISE  MO(_RAISE)
 #define ADJUST MO(_ADJUST2)
 #define ALTFN  TG(_ALTFN)
@@ -46,30 +48,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT_reviung41(
-    KC_F1,   KC_HOME, KC_UP,   KC_END,  KC_WH_U, XXXXXXX,          XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX, KC_DEL,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_WH_D, XXXXXXX,          XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, _______,
-    _______, KC_ESC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, KC_1,    KC_2,    KC_3,    KC_PDOT, _______,
+    KC_F1,   KC_HOME, KC_UP,   KC_END,  KC_WH_U, KC_CAPS,          KC_NUM,  KC_7,    KC_8,    KC_9,    KC_PMNS, KC_DEL,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_WH_D, XXXXXXX,          XXXXXXX, KC_4,    KC_5,    KC_6,    KC_PPLS, _______,
+    _______, KC_ESC,  XXXXXXX, XXXXXXX, NUMPAD,  XXXXXXX,          XXXXXXX, KC_1,    KC_2,    KC_3,    KC_PDOT, _______,
                                         _______, _______, KC_0,    _______, _______
+  ),
+
+  [_NUMPAD] = LAYOUT_reviung41(
+    _______, _______, _______, _______, _______, _______,          _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
+    _______, _______, _______, _______, _______, _______,          _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
+                                        _______, _______, KC_P0,   _______, _______
   ),
 
   [_RAISE] = LAYOUT_reviung41(
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_PERC, XXXXXXX,          XXXXXXX, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL,  KC_DEL,
     _______, KC_AMPR, KC_ASTR, KC_DLR,  XXXXXXX, XXXXXXX,          XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS, KC_QUOT, _______,
-    _______, XXXXXXX, KC_CIRC, KC_CIRC, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, KC_CIRC, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, _______,
                                         _______, _______, _______, _______, _______
   ),
 
   [_ADJUST] = LAYOUT_reviung41(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     _______, KC_SEC1, KC_SEC2, RGB_TOG, RGB_MOD, RGB_VAI,          RGB_HUI, RGB_SAI, RGB_SPI, _______, NK_TOGG, KC_PSCR,
-    _______, ALTFN,   KC_SEC3, KC_MUTE, KC_VOLD, KC_VOLU,          KC_MRWD, KC_MFFD, KC_MPLY, _______, DEBUG,   _______,
+    _______, ALTFN,   _______, KC_MUTE, KC_VOLD, KC_VOLU,          KC_MRWD, KC_MFFD, KC_MPLY, _______, DEBUG,   _______,
                                         _______, _______, QK_BOOT, _______, _______
   ),
 
   [_ADJUST2] = LAYOUT_reviung41(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     _______, KC_SEC1, KC_SEC2, RGB_TOG, RGB_MOD, RGB_VAI,          RGB_HUI, RGB_SAI, RGB_SPI, _______, NK_TOGG, KC_PSCR,
-    _______, ALTFN,   KC_SEC3, KC_MUTE, KC_VOLD, KC_VOLU,          KC_MRWD, KC_MFFD, KC_MPLY, _______, DEBUG,   _______,
+    _______, ALTFN,   _______, KC_MUTE, KC_VOLD, KC_VOLU,          KC_MRWD, KC_MFFD, KC_MPLY, _______, DEBUG,   _______,
                                         _______, _______, QK_BOOT, _______, _______
   ),
 };
