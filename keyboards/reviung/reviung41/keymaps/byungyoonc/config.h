0777794d745d78bc7eb6e2cc596adbc279ce3135
Choi Byungyoon <byungyoonc@gmail.com>
2023-02-07 09:28:17 +0900

    Personal keymap update on Technik-O, Planck, Reviung41

--- keyboards/reviung/reviung41/keymaps/byungyoonc/config.h
+++ keyboards/reviung/reviung41/keymaps/byungyoonc/config.h
@@ -87,6 +87,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEBOUNCE 40
 #endif
 
+#define IGNORE_MOD_TAP_INTERRUPT
+
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
 
