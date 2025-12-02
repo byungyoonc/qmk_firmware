5f4a0f75885392de773a5c9f1be84a9f56f1e3c3
Choi Byungyoon <byungyoonc@gmail.com>
2025-12-02 14:41:30 +0900

    Add RP2040 controller code for Boardsource Microdox

---
+++ keyboards/boardsource/microdox/rp2040/config.h
@@ -0,0 +1,10 @@
+// Copyright 2022 jack (@waffle87)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
+// #define I2C_DRIVER I2CD1
+// #define I2C1_SDA_PIN GP22
+// #define I2C1_SCL_PIN GP23
+#define RGB_MATRIX_LED_COUNT 38
+#define WS2812_DI_PIN GP9
