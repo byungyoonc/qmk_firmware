2b507e12270eb5044f3138d1f68e203dc04cdb58
Choi Byungyoon <byungyoonc@gmail.com>
2025-12-22 08:13:41 +0900

    Add keyboard Technik_MIT

---
+++ keyboards/technik_mit/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 Choi Byungyoon (@Choi Byungyoon)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
